f19362085a51b63a609bb72626794aac7178f5c3
Gregory Perifanos <g.perifanos89@gmail.com>
2023-12-11 20:30:22 +0200

    sayHello method

--- src/gr/aueb/cfproj5/Main.java
+++ src/gr/aueb/cfproj5/Main.java
@@ -2,6 +2,6 @@ package gr.aueb.cfproj5;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello World");
+        System.out.println("hello World!!!");
     }
 }
